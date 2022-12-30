a81fb508d77230c6264d5b17fbde73885321a66f
jdsteele <jdsteelepersonal@gmail.com>
2022-12-29 16:47:13 -0800

    stopwatch counts up

--- src/App.js
+++ src/App.js
@@ -9,10 +9,34 @@ function App() {
 
   const [time, setTime] = useState({ms:0, s:0, m:0, h:0})
 
-  const start = () => console.log("hello from start arrow function")
+  const start = () => {
+    run();
+    setInterval(run, 10);
+  }
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
 
 
-
+  }
   return (
     <div className="main-section">
       <div className="clock-holder">
